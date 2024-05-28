813e7e3b10d01fd1338fa37224b5c407d84fc02e
r4f705 <nikos.raftogiannis@gmail.com>
2024-05-28 23:37:16 +0300

    updated image version

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker rm -f miniature-dollop 
 docker image rm -f miniature-dollop
-docker build --tag miniature-dollop -f Dockerfile . 
-docker run -p 4000:4000 --rm --name=miniature-dollop miniature-dollop 
+docker build --tag miniature-dollop -f Dockerfile .
