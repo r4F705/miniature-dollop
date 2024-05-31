f88a7f99d249d79c6dbbb4ba60c6ff41043b6cb5
r4f705 <nikos.raftogiannis@gmail.com>
2024-05-31 11:28:00 +0300

    updated version to 0.7

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Super Duper Gophers!")
+	fmt.Fprint(w, "Hello, ULTRA Super Duper Gophers!")
 }
